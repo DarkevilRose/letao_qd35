29c329bf9d218822d3f89c9f6986e291d2769f5c
DarkevilRose <350683591@qq.com>
2019-02-23 16:04:49 +0800

    登录模块-完成进度条功能

--- public/back/js/login.js
+++ public/back/js/login.js
@@ -1,5 +1,7 @@
-// 校验规则：
+$(function(){
 
+
+// 校验规则：
 // 1. 用户名不能为空, 长度为2-6位
 // 2. 用户密码不能为空, 密码长度为6-12位 
 
@@ -28,7 +30,11 @@ $('#form').bootstrapValidator({
                 min: 2,
                 max: 6,
                 message:'用户名长度为2-6位'
-            }
+            },
+            // callback 专门用于配置回调提示消息
+            callback: {
+                message: '用户名不存在'
+          }
         }
         },
         //密码
@@ -44,12 +50,14 @@ $('#form').bootstrapValidator({
                 min: 6,
                 max: 12,
                 message:'密码长度必须是6-12'
+            },
+            callback: {
+                message:'密码错误'
             }
         }            
         }
     }
 });
-
 
 
 /* 
@@ -71,10 +79,10 @@ $('#form').on('success.form.bv', function(e){
         success:function(info){
             console.log(info);
             if (info.error == 1000) {
-                alert('用户名不存在');
+              $('#form').data('bootstrapValidator').updateStatus('username','INVALID','callback');
             }
             if (info.error ==1001) {
-                alert('密码错误');
+                $('#form').data('bootstrapValidator').updateStatus('password','INVALID','callback');
             }
             if (info.success) {
                 //登录成功,跳转页面
@@ -82,5 +90,18 @@ $('#form').on('success.form.bv', function(e){
             }
         }
     })
-})
+});
 
+    /*
+        3. 表单重置功能
+           reset 按钮, 本身就可以重置内容, 所以此时只需要重置状态即可
+           resetForm(false);  只重置状态
+           resetForm(true);   重置内容和状态
+    
+    */ 
+    
+    $('[type="reset"]').click (function(){
+            //重置状态即可
+            $('#form').data('bootstrapValidator').resetForm();
+    })  
+});
